672121f979e122c7a883b1fd999b45544c5c8657
jamilseq <aguilaryusakjamil@gmail.com>
2022-05-18 13:22:22 -0600

    realice mi ejercicio

---
+++ ejercicio5.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, resultado;
+            Console.WriteLine("Ingrese el valor de x :");
+            x = Convert.ToDouble(Console.ReadLine());
+            resultado = (3 * Math.Pow(x, 2)) + (7 * x) - 15;
+            Console.WriteLine("Resultado:{0}", resultado);
+            Console.ReadLine();
+
+        }
+    }
+}
